73b83c45091aae043db83f612f6a1bf8d135cbf5
Markus Schiffer <47703802+MarkusSchiffer@users.noreply.github.com>
2020-07-28 12:02:39 -0700

    routing changes

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -5,12 +5,12 @@ import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch,
 import NuxtLoading from './components/nuxt-loading.vue'
 import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
-import '..\\node_modules\\bootstrap\\dist\\css\\bootstrap.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
-import '..\\node_modules\\bootstrap-vue\\dist\\bootstrap-vue.css'
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 
-import _6f6c098b from '..\\layouts\\default.vue'
-import _6d5cfa3e from '..\\layouts\\secondary.vue'
+import _6f6c098b from '../layouts/default.vue'
+import _6d5cfa3e from '../layouts/secondary.vue'
 
 const layouts = { "_default": sanitizeComponent(_6f6c098b),"_secondary": sanitizeComponent(_6d5cfa3e) }
 
--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,13 +3,13 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _530b8798 = () => interopDefault(import('..\\pages\\data.vue' /* webpackChunkName: "pages/data" */))
-const _144124c2 = () => interopDefault(import('..\\pages\\map.vue' /* webpackChunkName: "pages/map" */))
-const _8209ca46 = () => interopDefault(import('..\\pages\\news.vue' /* webpackChunkName: "pages/news" */))
-const _3f6cd7b5 = () => interopDefault(import('..\\pages\\partners.vue' /* webpackChunkName: "pages/partners" */))
-const _60339019 = () => interopDefault(import('..\\pages\\people.vue' /* webpackChunkName: "pages/people" */))
-const _6dc6b404 = () => interopDefault(import('..\\pages\\projects.vue' /* webpackChunkName: "pages/projects" */))
-const _3e16fc90 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _586dd2ca = () => interopDefault(import('../pages/data.vue' /* webpackChunkName: "pages/data" */))
+const _b6553928 = () => interopDefault(import('../pages/map.vue' /* webpackChunkName: "pages/map" */))
+const _40eeb173 = () => interopDefault(import('../pages/news.vue' /* webpackChunkName: "pages/news" */))
+const _4c31996a = () => interopDefault(import('../pages/partners.vue' /* webpackChunkName: "pages/partners" */))
+const _339bd92f = () => interopDefault(import('../pages/people.vue' /* webpackChunkName: "pages/people" */))
+const _08410f9a = () => interopDefault(import('../pages/projects.vue' /* webpackChunkName: "pages/projects" */))
+const _c518843c = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -29,31 +29,31 @@ export const routerOptions = {
 
   routes: [{
     path: "/data",
-    component: _530b8798,
+    component: _586dd2ca,
     name: "data"
   }, {
     path: "/map",
-    component: _144124c2,
+    component: _b6553928,
     name: "map"
   }, {
     path: "/news",
-    component: _8209ca46,
+    component: _40eeb173,
     name: "news"
   }, {
     path: "/partners",
-    component: _3f6cd7b5,
+    component: _4c31996a,
     name: "partners"
   }, {
     path: "/people",
-    component: _60339019,
+    component: _339bd92f,
     name: "people"
   }, {
     path: "/projects",
-    component: _6dc6b404,
+    component: _08410f9a,
     name: "projects"
   }, {
     path: "/",
-    component: _3e16fc90,
+    component: _c518843c,
     name: "index"
   }],
 
