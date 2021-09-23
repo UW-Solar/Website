5d2425779c9af2c70258188516a91fcb7a0c8c23
Markus Schiffer <47703802+MarkusSchiffer@users.noreply.github.com>
2021-09-22 22:24:47 -0700

    rebuilt for updated npm version

--- dist/_nuxt/static/1620247044/map/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620247044",layout:"secondary",error:a,serverRendered:true,routePath:"\u002Fmap",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1ccbe11"}}}}(null));
--- dist/_nuxt/static/1620247044/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620247044",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1ccbe11"}}}}(null,"\u002F"));
