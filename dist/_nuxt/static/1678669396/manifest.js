892d2f189aabdf3099ab2815d40af73454ee46e9
Amanda Yuan <amandayuan2002@gmail.com>
2023-03-12 18:13:54 -0700

    Manastash Ridge new post update

--- dist/_nuxt/static/1678669396/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdata","\u002Fprojects","\u002Fpeople","\u002Fnews","\u002Fmap"]})
---
+++ dist/_nuxt/static/1678670014/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1678670014",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"11b49ed0"}}}}(null,"\u002F"));
